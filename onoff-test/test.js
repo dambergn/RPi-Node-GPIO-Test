9a02753c0780884b5af095a7937cb468ac64c3a6
dambergn <dambergn@gmail.com>
2018-04-04 13:08:06 -0700

    added some console logs to onoff test

--- onoff-test/test.js
+++ onoff-test/test.js
@@ -7,8 +7,10 @@ var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function ever
 function blinkLED() { //function to start blinking
   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
     LED.writeSync(1); //set pin state to 1 (turn LED on)
+    console.log('LED on');
   } else {
     LED.writeSync(0); //set pin state to 0 (turn LED off)
+    console.log('LED off');
   }
 }
 
@@ -16,6 +18,7 @@ function endBlink() { //function to stop blinking
   clearInterval(blinkInterval); // Stop blink intervals
   LED.writeSync(0); // Turn LED off
   LED.unexport(); // Unexport GPIO to free resources
+  console.log('Blink finished.');
 }
 
 setTimeout(endBlink, 5000); //stop blinking after 5 seconds
