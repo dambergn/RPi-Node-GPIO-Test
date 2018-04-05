c6bf9eb15fc0e2cc2b5e356525decfed0f6a882e
dambergn <dambergn@gmail.com>
2018-04-04 19:21:43 -0700

    changed on off test.

--- onoff-test/Button-onoff-test.js
+++ onoff-test/Button-onoff-test.js
@@ -3,17 +3,8 @@
 const Gpio = require('onoff').Gpio;
 const led = new Gpio(18, 'out');
 const button = new Gpio(4, 'in', 'both');
-let onOff = 0;
 
 button.watch(function (err, value) {
   console.log('Button Pressed');
-  if(onOff = 0) {
-    onOff = 1;
-  } else {
-    onOff = 0;
-  }
-
-  if(onOff = 1) {
-    led.writeSync(value);
-  }
+  led.writeSync(led.readSync(value) ^ 1); // 1 = on, 0 = off :)
 });
