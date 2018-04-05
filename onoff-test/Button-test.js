88bfcc4e14b96ca5c900403a711631f016c097f5
dambergn <dambergn@gmail.com>
2018-04-04 18:51:09 -0700

    added a console log for button pressed.

--- onoff-test/Button-test.js
+++ onoff-test/Button-test.js
@@ -5,5 +5,19 @@ const led = new Gpio(18, 'out');
 const button = new Gpio(4, 'in', 'both');
 
 button.watch(function (err, value) {
+  console.log('Button Pressed');
   led.writeSync(value);
 });
+
+// button.watch(function (err, value) {
+//   if (err) {
+//     throw err;
+//   }
+
+//   led.writeSync(value);
+// });
+
+// process.on('SIGINT', function () {
+//   led.unexport();
+//   button.unexport();
+// });
